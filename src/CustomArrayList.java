fa9ccf165e8046310b553fe5a74f8d0c4228471b
saul813 <saulsabunyo8@gmail.com>
2026-06-27 11:49:17 +0300

    Add Custom Arrayist

--- src/CustomArrayList.java
+++ src/CustomArrayList.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 class CourseArrayList {
     public int[] data ; //Underlying array to store data
     public int size;  // Current actual number of elements in our list.
@@ -12,13 +14,56 @@ class CourseArrayList {
     //Adding an element to the array
     public void addElement(int element){
         if(size == capacity){
-            throw new ArrayIndexOutOfBoundsException("The Course is full");
+            resizeCourse();
         }
         data[size] = element;
         size++;
     }
 
+    //Resizing the Course to increase the capacity
+    public void resizeCourse() {
+        //Doubling the capacity of the course
+        capacity *= 2;
+
+        //Creating a new array to take up the current capacity
+        int newData[] = new int[capacity];
+
+        //Copying elements of the old array to the new array
+        for(int i = 0; i < data.length; i++){
+            newData[i] = data[i];
+        }
+        //Replacing the old array with the new array
+        data = newData;
+        System.out.println("The Course is full and it's Capacity has been increased to "+ capacity);
+    }
+
+    //Get an element at a particular index
+    public int getElement(int index){
+        if(index < 0 || index > size){
+            throw new ArrayIndexOutOfBoundsException("Invalid index");
+        }
+        return data[index];
+    }
+
     //
 }
 public class CustomArrayList {
+    public static void main(String[] args) {
+        CourseArrayList ca = new CourseArrayList();
+        ca.addElement(1);
+        int returnedElement0 = ca.getElement(0);
+        System.out.println("Returned Element is "+returnedElement0);
+        ca.addElement(2);
+        int returnedElement1 = ca.getElement(1);
+        System.out.println("Returned Element is "+returnedElement1);
+        ca.addElement(3);
+        int returnedElement2 = ca.getElement(2);
+        System.out.println("Returned Element is "+returnedElement2);
+        ca.addElement(4);
+        int returnedElement3 = ca.getElement(3);
+        System.out.println("Returned Element is "+returnedElement3);
+        ca.addElement(5);
+        int returnedElement4 = ca.getElement(4);
+        System.out.println("Returned Element is "+returnedElement4);
+    }
 }
