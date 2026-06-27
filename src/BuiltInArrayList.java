367db6238ece12126bb8dcf8a5888becc5ae7e6e
saul813 <saulsabunyo8@gmail.com>
2026-06-27 12:06:43 +0300

    Add implementation of built in ArrayList

--- src/BuiltInArrayList.java
+++ src/BuiltInArrayList.java
@@ -1,2 +1,20 @@
+import java.util.*;
 public class BuiltInArrayList {
+    public static void main(String[] args){
+
+        //Creating the ArrayList
+        ArrayList<String> Students = new ArrayList<>();
+
+        //Adding an Element to the ArrayList
+        Students.add("Sabunyo Saul");
+
+        //Getting an Element from the arrayList
+        String elementInList = Students.get(0);
+        System.out.println("Element added " + elementInList);
+
+        //Updating an Element in the List
+        Students.set(0, "Masaba Daniel");
+        String updatedElement = Students.get(0);
+        System.out.println("Element Updated to " + updatedElement);
+    }
 }
