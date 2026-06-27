f5d85887346b3b653c8a2fa8236456834cdbc8a1
saul813 <saulsabunyo8@gmail.com>
2026-06-27 12:19:38 +0300

    Add a for each loop to print the elements in the arrayList

--- src/BuiltInArrayList.java
+++ src/BuiltInArrayList.java
@@ -16,5 +16,24 @@ public class BuiltInArrayList {
         Students.set(0, "Masaba Daniel");
         String updatedElement = Students.get(0);
         System.out.println("Element Updated to " + updatedElement);
+
+        Students.add("SS Saul");
+        Students.add("Weaponz Saul");
+        Students.add("Wpnz Saul");
+        Students.add("Sab Saul");
+
+        //Check the total number of items using .size()
+        System.out.println("Total items: " + Students.size());
+
+        //Remove an item using .remove(index)
+        Students.remove(0);
+
+        //Printing all elements in the ArrayList
+        System.out.println("\n\n List Of Students");
+        int i = 0;
+        for(String student : Students){
+            System.out.println((i+1)+ ". " + student);
+            i++;
+        }
     }
 }
